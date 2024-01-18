96250d716113dc7576f5d0920c42cd36bdc6f3ad
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-01-18 23:46:39 +0530

    Solved few bugs

--- portfolioAPI/blog/views.py
+++ portfolioAPI/blog/views.py
@@ -18,6 +18,16 @@ class BlogViewSet(viewsets.ModelViewSet):
         return Response({'status': status.HTTP_200_OK, 'message':'Success'})
 
     @action(detail=True, methods=['POST'])
+    def dislike(self, request, pk=None):
+        blog = self.get_object()
+        if blog.likes > 0:
+            blog.likes -= 1
+            blog.save()
+            return Response({'status': status.HTTP_200_OK, 'message':'Success'})
+        else:
+            return Response({'status': status.HTTP_400_BAD_REQUEST, 'message':'Bad Request'})
+
+    @action(detail=True, methods=['POST'])
     def comment(self, request, pk=None):
         blog = self.get_object()
         serializer = CommentSerializer(data=request.data)
